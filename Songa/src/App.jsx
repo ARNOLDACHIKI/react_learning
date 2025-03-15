1707402f4e7155c8a6ffaca90a183f74b3a9c063
ARNOLD <achikiarnold@gmail.com>
2025-03-15 13:09:56 +0300

    kk

---
+++ Songa/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+function App() {
+ return (
+  <>
+  <navbar />
+  <navbar />
+  <h1>Main component</h1>
+    </>
+  )
+}
+
+export default App
---
+++ Songa/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+
+import '../App.css'
+
+
+function Navbar() {
+  
+
+  return (
+    <>
+      
+      <div className="container">
+        <div><h1>logo</h1></div>
+        <div>react</div>
+        <div>
+          <ul>
+            <li>home</li>
+            <li>about</li>
+            <li>contact</li>
+            <li>Projects</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Navbar
