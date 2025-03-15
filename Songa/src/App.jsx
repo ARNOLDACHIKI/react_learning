273e31eb5ee4de2e3e424bd65a0ddd2387cd4c91
ARNOLD <achikiarnold@gmail.com>
2025-03-15 15:18:55 +0300

    add Footer component to App and remove duplicate Navbar

--- Songa/src/App.jsx
+++ Songa/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import Footer from './components/Footer.jsx';
 import Navbar from './components/Navbar.jsx';
 function App() {
  return (
   <>
   <Navbar />
-  <Navbar />
+  
+  
   <h1>Main component</h1>
+  <Footer />
     </>
   )
 }
